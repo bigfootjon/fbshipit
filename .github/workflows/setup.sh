9b1fa51d1e1b2e79adc4cd83b783d5bdc4da7746
Jon Janzen <jjjonjanzen@gmail.com>
2020-04-02 18:35:28 -0700

    /

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -15,11 +15,8 @@ apt-get install -y \
 
 locale-gen en_US.UTF-8
 export LC_ALL=en_US.UTF-8
-export HGENCODING=utf-8
 
-echo $LC_ALL
-echo $LANG
-echo $HGENCODING
+locale
 
 git --version
 hg --version
