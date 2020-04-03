0112f1c4f7d8aa808a23db9de3c6d98076f64fcf
Jon Janzen <jjjonjanzen@gmail.com>
2020-04-02 19:38:29 -0700

    -

--- .github/workflows/setup.sh
+++ .github/workflows/setup.sh
@@ -22,11 +22,4 @@ hhvm --version
 
 curl https://getcomposer.org/installer | php -- /dev/stdin --install-dir=/usr/local/bin --filename=composer
 
-rm -rf .gitignore .github .hhconfig
-rm -rf *
-ls -la
-git clone https://github.com/bigfootjon/fbshipit.git .
-git checkout gh-actions
-
 php /usr/local/bin/composer install
-
